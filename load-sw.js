103bbc54a518561e5df62b668b9ec62c758c6148
Saturn-VI <maxwang420@gmail.com>
2026-03-19 11:32:43 -0500

    fix syntax error (redeclaration of const)

--- load-sw.js
+++ load-sw.js
@@ -1,4 +1,5 @@
-const serviceWorker = '/sw.js'
+const serviceWorker = '/sw.js';
+// In the iOS and Android apps, a special user agent is set to identify whether this is running in the app or a regular browser.
 const isWebView = /CSABuddy/.test(navigator.userAgent);
 
 // don't use for android/ios app - they already have pages saved
